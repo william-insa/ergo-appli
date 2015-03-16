640a8c54cda505a4a97e46bea6af04a198e9390c
bseznec <blandine.seznec@insa-rennes.fr>
2015-03-16 22:28:03 +0100

    page principale

---
+++ MyDomotik/MyDomotik/Device.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDomotik
+{
+    class Device
+    {
+        // Nom de l'appareil
+        private string deviceName;
+        // Chemin de l'icone
+        private string icone;
+
+        public Device(string name)
+        {
+            this.deviceName = name;
+            this.icone = "assets/device_default_icone.png";
+        
+        }
+        public void setDeviceName(string name)
+        {
+            this.deviceName = name;
+        }
+
+        public string getRoomName()
+        {
+            return this.deviceName;
+        }
+
+        public void setIcone(string icone)
+        {
+            this.icone = icone;
+        }
+
+        public string getIcone()
+        {
+            return this.icone;
+        }
+    }
+}
--- MyDomotik/MyDomotik/MainPage.xaml.cs
+++ MyDomotik/MyDomotik/MainPage.xaml.cs
@@ -1,10 +1,10 @@
 ﻿using System;
-using System.Threading;
-using System.Globalization;
 using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -15,24 +15,24 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// Pour en savoir plus sur le modèle d'élément Page vierge, consultez la page http://go.microsoft.com/fwlink/?LinkId=234238
 
 namespace MyDomotik
 {
     /// <summary>
     /// Une page vide peut être utilisée seule ou constituer une page de destination au sein d'un frame.
     /// </summary>
+    /// 
+   
     public sealed partial class MainPage : Page
     {
+        //public static Room[] rooms;
+        
         public MainPage()
         {
             this.InitializeComponent();
             this.displayTime();
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-
+            //this.displayRooms();
+ 
         }
 
         private void displayTime()
@@ -40,10 +40,12 @@ namespace MyDomotik
             TimeBox.Text = DateTime.Now.ToString();
         }
 
-        private void TimeBox_Copy_TextChanged(object sender, TextChangedEventArgs e)
-        {
+     
 
-        }
+       /* private void displayRooms()
+        {
+          TO DO
+        }*/
 
       
        
