f77f041e3cce58fe132ed32fba45a1b729e56b6c
william-insa <william.dazi@insa-rennes.fr>
2015-05-02 19:54:16 +0200

    Menu AdminPage

--- MyDomotik/AdminPage.xaml.cs
+++ MyDomotik/AdminPage.xaml.cs
@@ -22,6 +22,7 @@ namespace MyDomotik
     /// </summary>
     public sealed partial class AdminPage : Page
     {
+
         public AdminPage()
         {
             this.InitializeComponent();
@@ -37,19 +38,77 @@ namespace MyDomotik
             this.Frame.Navigate(typeof(AdminPage));
         }
 
-        private void gestionIcones(object sender, RoutedEventArgs e)
+        private void gestionIcones(object sender, RoutedEventArgs e)  // ramplace les boutons du menu Menu par le menu Gestion icônes
         {
-            this.Frame.Navigate(typeof(GestionIcones));
+            admin_buttons_grid.Children.RemoveAt(3);
+            admin_buttons_grid.Children.RemoveAt(2);
+
+            RowDefinitionCollection defs = admin_buttons_grid.RowDefinitions;
+            defs.RemoveAt(2);
+            defs.RemoveAt(1);
+
+            admin_button_1.Content = "Gestion des pièces";
+            admin_button_2.Content = "Gestion des équipements";
+
+            admin_button_1.Click -= accesParamInterface;
+            admin_button_1.Click += accesGestionPieces;
+
+            admin_button_2.Click -= gestionIcones;
+            admin_button_2.Click += accesGestionEquip;
+
+            admin_button_3.Click -= accesParamInterface;
+            admin_button_3.Click += accesPageReseau;
+        }
+
+
+        private void accesParamInterface(object sender, RoutedEventArgs e)
+        {
+            admin_buttons_grid.Children.RemoveAt(3);
+            
+            RowDefinitionCollection defs = admin_buttons_grid.RowDefinitions;
+            defs.RemoveAt(2);
+
+            admin_button_1.Content = "Couleurs";
+            admin_button_2.Content = "Tailles icônes";
+            admin_button_3.Content = "Mode défilement";
+
+            admin_button_1.Click -= accesParamInterface;
+            admin_button_1.Click += accesParamCouleur;
+            
+            admin_button_2.Click -= gestionIcones;
+            admin_button_2.Click += accesParamTaille;
+
+            admin_button_3.Click -= accesParamInterface;
+            admin_button_3.Click += accesPageReseau;
+        }
+
+        private void accesParamCouleur(object sender, RoutedEventArgs e)
+        {
+            // Frame.Navigate(typeof(ReglagesCouleurs));
+        }
+        private void accesParamTaille(object sender, RoutedEventArgs e)
+        {
+           // Frame.Navigate(typeof(ReglagesTailleIcones));
+
+        }
+        private void accesParamDefil(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(ReglagesModeSelection));
+        }
+
+        private void accesGestionPieces(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(GestionIcones));
+        }
+
+        private void accesGestionEquip(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(GestionIcones2));
         }
 
         private void accesPageReseau(object sender, RoutedEventArgs e)
         {
             this.Frame.Navigate(typeof(ReglagesReseau));
-        }
-
-        private void accesParamInterface(object sender, RoutedEventArgs e)
-        {
-            Frame.Navigate(typeof(ParamInterface));
         }
     }
 }
